44076785355eacdcbd94e550a5a63e8c1d10beab
Daniil <DozDi@yandex.ru>
2023-10-21 17:45:09 +0300

    add scan package

--- src/main/java/com/doza/springcore/SpringcoreApplication.java
+++ src/main/java/com/doza/springcore/SpringcoreApplication.java
@@ -3,7 +3,10 @@ package com.doza.springcore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"com.doza.springcore",
+							"com.doza.util"}
+)
 public class SpringcoreApplication {
 
 	public static void main(String[] args) {
